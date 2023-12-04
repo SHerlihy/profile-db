0060aab23d360a242890e3ce551435abeb668bcf
SHerlihy <steven_herlihy@yahoo.com>
2023-12-04 01:17:54 +0000

    tidy redis cli removal

--- deployment/proxy/main.tf
+++ deployment/proxy/main.tf
@@ -43,33 +43,6 @@ resource "aws_instance" "redis-ec2" {
   key_name = aws_key_pair.profile_proxy.key_name
 }
 
-#resource "terraform_data" "provision_server" {
-#  connection {
-#    type = "ssh"
-#    port = "22"
-#
-#    host = aws_instance.redis-ec2.public_ip
-#    user = "ec2-user"
-#
-#    private_key = file("./.ssh/id_rsa")
-#
-#    timeout = "2m"
-#  }
-#
-#    #  provisioner "file" {
-#    #    source      = "./redis-cli.sh"
-#    #    destination = "/tmp/redis-cli.sh"
-#    #  }
-#    #
-#    #  provisioner "remote-exec" {
-#    #    inline = [
-#    #      "chmod +x /tmp/redis-cli.sh",
-#    #      "/tmp/redis-cli.sh",
-#    #      # "nohup /home/ec2-user/redis-stable/src/redis-cli -u rediss://${var.user_name}:${var.user_pass}@${aws_instance.redis-ec2.public_ip}:6379/0 > /dev/nul 2>&1 &"
-#    #    ]
-#    #  }
-#}
-
 output "ec2_ip" {
   value = aws_instance.redis-ec2.public_ip
 }
